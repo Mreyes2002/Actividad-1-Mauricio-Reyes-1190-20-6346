5d3cca2fa13020948752c7bae56898097a5835e0
Mauricio <mauricio2002reyescruz@gmail.com>
2024-07-20 18:06:21 -0600

    Se agrega la tarea#1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("registration-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Función para exportar a PDF no implementada.");
+});
